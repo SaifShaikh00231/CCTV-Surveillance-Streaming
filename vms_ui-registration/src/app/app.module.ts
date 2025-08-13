2a36aa76520f75cdf77f74cbce21be1a3e0af92a
SaifShaikh00231 <saifshaikh00231@gmail.com>
2025-08-13 23:36:22 +0530

    redirect added

--- vms_ui-registration/src/app/app.module.ts
+++ vms_ui-registration/src/app/app.module.ts
@@ -2,21 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
-import { FormsModule } from '@angular/forms';  // Import FormsModule
+import { FormsModule } from '@angular/forms';
 import { AuthInterceptor } from './auth.interceptor';
 import { LiveViewComponent } from './live-view/live-view.component';
 import { MatSelectModule } from '@angular/material/select';
 import { MatOptionModule } from '@angular/material/core';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
-
-
 import { AppComponent } from './app.component';
 import { HomeComponent } from './registration/home.component';
-import { ResetPasswordComponent } from './registration/resetpassword.component';  // Ensure ResetPasswordComponent is declared here
+import { ResetPasswordComponent } from './registration/resetpassword.component';
 import { CameraComponent } from './camera/camera.component';
-import { DatePipe } from '@angular/common';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';  // Import DatePipe
+import { DatePipe, LocationStrategy, HashLocationStrategy } from '@angular/common';  // <-- Import LocationStrategy and HashLocationStrategy
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
 import { NavbarComponentModule } from './navbar/navbar.component.module';
 import { UsersListComponent } from './users-list/users-list.component';
@@ -25,20 +23,45 @@ import { PermissionsComponent } from './permissions/permissions.component';
 import { PlaybackComponent } from './playback/playback.component';
 import { ReportsComponent } from './reports/reports.component';
 import { ProfileComponent } from './profile/profile.component';
-import {AddUsersComponent} from './add-users/add-users.component';
+import { AddUsersComponent } from './add-users/add-users.component';
 import { SettingsComponent } from './settings/settings.component';
 import { TabsComponent } from './tabs/tabs.component';
 
-
-
 @NgModule({
-  declarations: [AppComponent, HomeComponent, ResetPasswordComponent, CameraComponent, LiveViewComponent,
-    UsersListComponent, RolesComponent, PermissionsComponent, PlaybackComponent, ReportsComponent, ProfileComponent,AddUsersComponent, SettingsComponent,TabsComponent],  // Include ResetPasswordComponent
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule, BrowserAnimationsModule, MatIconModule, MatSelectModule,
-    MatOptionModule, MatMenuModule,
-    MatButtonModule, NavbarComponentModule,
-  ],  // Include FormsModule
-  providers: [DatePipe, { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }],  // Corrected providers array
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ResetPasswordComponent,
+    CameraComponent,
+    LiveViewComponent,
+    UsersListComponent,
+    RolesComponent,
+    PermissionsComponent,
+    PlaybackComponent,
+    ReportsComponent,
+    ProfileComponent,
+    AddUsersComponent,
+    SettingsComponent,
+    TabsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatMenuModule,
+    MatButtonModule,
+    NavbarComponentModule,
+  ],
+  providers: [
+    DatePipe,
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+    { provide: LocationStrategy, useClass: HashLocationStrategy }  // <-- Add this provider
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule { }
